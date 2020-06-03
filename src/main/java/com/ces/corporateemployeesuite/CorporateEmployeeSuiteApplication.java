9a4279fc64b62b215cb111bafcf98b13f8e2d8f7
Bhagyashree53 <bhagyashreebjp126@gmail.com>
2020-06-03 16:26:24 +0530

    Initial commit

---
+++ src/main/java/com/ces/corporateemployeesuite/CorporateEmployeeSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.ces.corporateemployeesuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateEmployeeSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateEmployeeSuiteApplication.class, args);
+	}
+
+}
