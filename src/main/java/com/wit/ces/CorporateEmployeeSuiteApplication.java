67e790e85d92d9b675acfa7b6b716141e621cbc8
Bhagyashree53 <bhagyashreebjp126@gmail.com>
2020-06-03 12:43:18 +0530

    Initial project structure commit

---
+++ src/main/java/com/wit/ces/CorporateEmployeeSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.wit.ces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporateEmployeeSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporateEmployeeSuiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wit/ces/CorporateEmployeeSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wit.ces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporateEmployeeSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
